18d2f36864dca6c6667017eac832309777d01931
modybon <61365506+modybon@users.noreply.github.com>
2022-10-10 22:40:46 -0400

    First Commit

---
+++ ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  FoodApp
+//
+//  Created by Mohamed Fahmy on 2022-10-10.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
