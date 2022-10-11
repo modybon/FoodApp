35fb8bc0e9acd445e7cd9e18a47ea022c450d75f
modybon <61365506+modybon@users.noreply.github.com>
2022-10-10 23:10:00 -0400

    Added MVC folders and created app views

---
+++ Views/BasketView.swift
@@ -0,0 +1,20 @@
+//
+//  BasketView.swift
+//  FoodApp
+//
+//  Created by Mohamed Fahmy on 2022-10-10.
+//
+
+import SwiftUI
+
+struct BasketView: View {
+    var body: some View {
+        Text("Basket")
+    }
+}
+
+struct BasketView_Previews: PreviewProvider {
+    static var previews: some View {
+        BasketView()
+    }
+}
---
+++ Views/OrdersView.swift
@@ -0,0 +1,20 @@
+//
+//  OrdersView.swift
+//  FoodApp
+//
+//  Created by Mohamed Fahmy on 2022-10-10.
+//
+
+import SwiftUI
+
+struct OrdersView: View {
+    var body: some View {
+        Text("Orders")
+    }
+}
+
+struct OrdersView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrdersView()
+    }
+}
